34213dbd5cdd65b8469534654bc84509f07ac258
Jannek <jannekvieth@outlook.de>
2020-07-30 17:36:13 +0200

    Addition
    + Enemy Prefab
    + EnemyArrow Prefab
    + EnemySpawner Prefab
    +Enemy Arrow Script
    + Enemy Arrow Sprite

---
+++ Praktisch/For Kingdom And Glory/Assets/Scripts/Enemy/EnemyArrow.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyArrow : MonoBehaviour
+{
+    [SerializeField]
+    [Range(5, 20)]
+    private int m_Damage = 5;
+
+    private float m_Timer = 0.0f;
+    private void Update()
+    {
+        m_Timer += Time.deltaTime;
+
+        if (m_Timer >= 2.5f)
+            Destroy(this.gameObject);
+    }
+
+    #region Collision Function
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Wall"))
+        {
+            collision.gameObject.GetComponent<Wall>().GetDamage(m_Damage);
+
+            Destroy(this.gameObject);
+        }
+        else if (collision.CompareTag("Player"))
+        {
+
+        }
+        else if (collision.CompareTag("Villager"))
+        {
+            collision.GetComponent<VagrantBehaviour>().StepDown();
+
+            Destroy(this.gameObject);
+        }
+        else if (collision.CompareTag("Archer"))
+        {
+            collision.GetComponent<VagrantBehaviour>().StepDown();
+
+            Destroy(this.gameObject);
+        }
+        else if (collision.CompareTag("Builder"))
+        {
+            collision.GetComponent<VagrantBehaviour>().StepDown();
+
+            Destroy(this.gameObject);
+        }
+    }
+    #endregion
+}

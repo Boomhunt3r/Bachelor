6d5a5a7b75aa2e87c6b2126cefdc59d72e0ec605
Jannek <jannekvieth@outlook.de>
2020-12-02 17:31:31 +0100

    Changelog
    + Sorted out some Functions
    ~ Working on new Repair Armor function

---
+++ Praktisch/For Kingdom And Glory/Assets/Scripts/Player/PlayerBehaviour.PrivateFunctions.cs
@@ -0,0 +1,178 @@
+﻿using UnityEngine;
+
+public partial class PlayerBehaviour : MonoBehaviour
+{
+    #region private functions
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="_Direction">Direction Player is looking</param>
+    private void Shoot(Vector2 _Direction)
+    {
+        GameObject Target = null;
+        GameObject Arrow = null;
+
+        float XDistance = 0.0f;
+        float YDistance = 0.0f;
+        float ThrowAngle;
+        float TotalVelo;
+        float XVelo;
+        float YVelo;
+
+        Target = GetClosestTarget(GameObject.FindGameObjectsWithTag("Enemy"), GameObject.FindGameObjectsWithTag("Rabbit"));
+
+        Debug.Log(Vector2.Distance(Target.transform.position, transform.position));
+
+        if (Target != null && Vector2.Distance(Target.transform.position, transform.position) <= 10.0f)
+        {
+            switch (Inventory.Instance.Bow)
+            {
+                case EPlayerUpgrade.WOOD:
+                    XDistance = Random.Range(Target.transform.position.x - m_ThrowPoint.transform.position.x, _Direction.x * 5.0f);
+
+                    YDistance = Random.Range(Target.transform.position.y - m_ThrowPoint.position.y, 5.0f);
+                    break;
+                case EPlayerUpgrade.STONE:
+                    XDistance = Random.Range(Target.transform.position.x - m_ThrowPoint.transform.position.x, _Direction.x * 2.5f);
+
+                    YDistance = Random.Range(Target.transform.position.y - m_ThrowPoint.position.y, 2.5f);
+                    break;
+                case EPlayerUpgrade.IRON:
+                    XDistance = Target.transform.position.x - m_ThrowPoint.transform.position.x;
+                    YDistance = Target.transform.position.y - m_ThrowPoint.transform.position.y;
+                    break;
+                default:
+                    break;
+            }
+
+            ThrowAngle = Mathf.Atan((YDistance + 4.905f) / XDistance);
+
+            TotalVelo = XDistance / Mathf.Cos(ThrowAngle);
+
+            XVelo = TotalVelo * Mathf.Cos(ThrowAngle);
+            YVelo = TotalVelo * Mathf.Sin(ThrowAngle);
+
+            Arrow = Instantiate(m_Arrow, m_ThrowPoint.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+            Arrow.GetComponent<Rigidbody2D>().velocity = new Vector2(XVelo, YVelo);
+
+            EffectSource.Play();
+
+            m_Timer = 0.0f;
+
+            return;
+        }
+
+        XDistance = Random.Range(_Direction.x * 2.5f, _Direction.x * 5.0f);
+
+        YDistance = Random.Range(transform.position.y + 10.0f - m_ThrowPoint.position.y, 5.0f);
+
+        ThrowAngle = Mathf.Atan((YDistance + 4.905f) / XDistance);
+
+        TotalVelo = XDistance / Mathf.Cos(ThrowAngle);
+
+        XVelo = TotalVelo * Mathf.Cos(ThrowAngle);
+        YVelo = TotalVelo * Mathf.Sin(ThrowAngle);
+
+        Arrow = Instantiate(m_Arrow, m_ThrowPoint.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+        Arrow.GetComponent<Rigidbody2D>().velocity = new Vector2(XVelo, YVelo);
+
+        EffectSource.Play();
+
+        m_Timer = 0.0f;
+    }
+
+    /// <summary>
+    /// Closest Target Function
+    /// </summary>
+    /// <param name="_Enemies">All Enemies</param>
+    /// <param name="_Rabbits">All Rabbits</param>
+    /// <returns>Closest Target</returns>
+    private GameObject GetClosestTarget(GameObject[] _Enemies, GameObject[] _Rabbits)
+    {
+        GameObject TargetE = null;
+        GameObject TargetR = null;
+        GameObject TargetO = null;
+
+        float MinDist = Mathf.Infinity;
+        float EDist = 0.0f;
+        float RDist = 0.0f;
+        float Dist = 0.0f;
+
+        if (_Enemies != null)
+        {
+            for (int i = 0; i < _Enemies.Length; i++)
+            {
+                Dist = Vector2.Distance(_Enemies[i].transform.position, transform.position);
+
+                if (Dist < MinDist)
+                {
+                    TargetE = _Enemies[i];
+                    MinDist = Dist;
+                    EDist = Dist;
+                }
+            }
+        }
+
+        MinDist = Mathf.Infinity;
+        Dist = 0.0f;
+
+        if (_Rabbits != null)
+        {
+            for (int i = 0; i < _Rabbits.Length; i++)
+            {
+                Dist = Vector2.Distance(_Rabbits[i].transform.position, transform.position);
+
+                if (Dist < MinDist)
+                {
+                    TargetR = _Rabbits[i];
+                    MinDist = Dist;
+                    RDist = Dist;
+                }
+            }
+        }
+
+        if (EDist <= RDist || RDist == 0.0f)
+        {
+            TargetO = TargetE;
+        }
+
+        if (EDist == 0.0f)
+        {
+            TargetO = TargetR;
+        }
+
+        return TargetO;
+    }
+
+    private void PayRepair(EPlayerUpgrade _Tier, ECraftingType _Type)
+    {
+        switch (_Tier)
+        {
+            case EPlayerUpgrade.STONE:
+                switch (_Type)
+                {
+                    case ECraftingType.HELMET:
+                        break;
+                    case ECraftingType.PLATE:
+                        break;
+                    case ECraftingType.BOOTS:
+                        break;
+                }
+                break;
+            case EPlayerUpgrade.IRON:
+                switch (_Type)
+                {
+                    case ECraftingType.HELMET:
+                        break;
+                    case ECraftingType.PLATE:
+                        break;
+                    case ECraftingType.BOOTS:
+                        break;
+                }
+                break;
+            default:
+                break;
+        }       
+    }
+    #endregion
+}
---
+++ Praktisch/For Kingdom And Glory/Assets/Scripts/Player/PlayerBehaviour.PublicFunction.cs
@@ -0,0 +1,205 @@
+﻿using UnityEngine;
+
+public partial class PlayerBehaviour : MonoBehaviour
+{
+    #region public functions
+    // TODO: Repair
+    /// <summary>
+    /// 
+    /// </summary>
+    public void RepairArmor()
+    {
+        int Dif = m_MaxArmor - m_Armor;
+
+        bool HelmetPayed = false;
+        bool PlatePayed = false;
+        bool BootsPayed = false;
+
+        switch (Inventory.Instance.Helmet)
+        {
+            case EPlayerUpgrade.STONE:
+                if (Inventory.Instance.Stone >= 3)
+                    HelmetPayed = true;
+                break;
+            case EPlayerUpgrade.IRON:
+                if (Inventory.Instance.Iron >= 3)
+                    HelmetPayed = true;
+                break;
+            default:
+                break;
+        }
+        switch (Inventory.Instance.Plate)
+        {
+            case EPlayerUpgrade.STONE:
+                if (Inventory.Instance.Stone >= 6)
+                    PlatePayed = true;
+                break;
+            case EPlayerUpgrade.IRON:
+                if (Inventory.Instance.Iron >= 6)
+                    PlatePayed = true;
+                break;
+            default:
+                break;
+        }
+        switch (Inventory.Instance.Boots)
+        {
+            case EPlayerUpgrade.STONE:
+                if (Inventory.Instance.Stone >= 2)
+                    BootsPayed = true;
+                break;
+            case EPlayerUpgrade.IRON:
+                if (Inventory.Instance.Iron >= 2)
+                    BootsPayed = true;
+                break;
+            default:
+                break;
+        }
+
+        if (!HelmetPayed && !PlatePayed && !BootsPayed)
+            return;
+
+        if(HelmetPayed && !PlatePayed && !BootsPayed ||
+           PlatePayed && !BootsPayed && !HelmetPayed ||
+           BootsPayed && !HelmetPayed && !PlatePayed)
+        {
+            if(HelmetPayed)
+            {
+                PayRepair(Inventory.Instance.Helmet, ECraftingType.HELMET);
+            }
+            if(PlatePayed)
+            {
+                PayRepair(Inventory.Instance.Plate, ECraftingType.PLATE);
+
+            }
+            if (BootsPayed)
+            {
+                PayRepair(Inventory.Instance.Boots, ECraftingType.BOOTS);
+            }
+        }
+
+        if(HelmetPayed && PlatePayed && !BootsPayed ||
+           HelmetPayed && BootsPayed && !PlatePayed ||
+           PlatePayed && BootsPayed && !HelmetPayed)
+        {
+            if(HelmetPayed && BootsPayed)
+            {
+                PayRepair(Inventory.Instance.Helmet, ECraftingType.HELMET);
+                PayRepair(Inventory.Instance.Boots, ECraftingType.BOOTS);
+            }
+            if(HelmetPayed && PlatePayed)
+            {
+                PayRepair(Inventory.Instance.Helmet, ECraftingType.HELMET);
+                PayRepair(Inventory.Instance.Plate, ECraftingType.PLATE);
+            }
+            if (PlatePayed && BootsPayed)
+            {
+                PayRepair(Inventory.Instance.Plate, ECraftingType.PLATE);
+                PayRepair(Inventory.Instance.Boots, ECraftingType.BOOTS);
+            }
+        }
+
+        if(HelmetPayed && PlatePayed && BootsPayed)
+        {
+            PayRepair(Inventory.Instance.Helmet, ECraftingType.HELMET);
+            PayRepair(Inventory.Instance.Plate, ECraftingType.PLATE);
+            PayRepair(Inventory.Instance.Boots, ECraftingType.BOOTS);
+        }
+
+        m_Armor = m_MaxArmor;
+    }
+
+    public void HealPlayer()
+    {
+        if (m_Health < m_MaxHealth)
+        {
+            if (Inventory.Instance.Coins >= 2)
+            {
+                m_Health = m_MaxHealth;
+                Inventory.Instance.Coins -= 2;
+            }
+        }
+    }
+
+    public void GetDamage(int _Amount)
+    {
+        if (m_Armor <= 0)
+        {
+            m_Health -= _Amount;
+        }
+        else if (m_Armor > 0)
+        {
+            m_Armor -= _Amount;
+        }
+
+        if (m_Health <= 0)
+        {
+            GameManager.Instance.IsAlive = false;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="_Armor">Armor being Upgraded</param>
+    public void UpgradeArmor(ECraftingType _Armor)
+    {
+        switch (_Armor)
+        {
+            case ECraftingType.HELMET:
+                switch (Inventory.Instance.Helmet)
+                {
+                    case EPlayerUpgrade.NONE:
+                        m_Helmet = 0;
+                        break;
+                    case EPlayerUpgrade.STONE:
+                        m_Helmet = 5;
+                        break;
+                    case EPlayerUpgrade.IRON:
+                        m_Helmet = 10;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case ECraftingType.PLATE:
+                switch (Inventory.Instance.Plate)
+                {
+                    case EPlayerUpgrade.NONE:
+                        m_Plate = 0;
+                        break;
+                    case EPlayerUpgrade.STONE:
+                        m_Plate = 10;
+                        break;
+                    case EPlayerUpgrade.IRON:
+                        m_Plate = 20;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case ECraftingType.BOOTS:
+                switch (Inventory.Instance.Boots)
+                {
+                    case EPlayerUpgrade.NONE:
+                        m_Boots = 0;
+                        break;
+                    case EPlayerUpgrade.STONE:
+                        m_Boots = 5;
+                        break;
+                    case EPlayerUpgrade.IRON:
+                        m_Boots = 10;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            default:
+                break;
+        }
+
+        m_MaxArmor = m_Helmet + m_Plate + m_Boots;
+
+        Armor = m_Helmet + m_Plate + m_Boots;
+    }
+    #endregion
+}

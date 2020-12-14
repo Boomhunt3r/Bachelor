ef4f16827c87035c8044e08179b2e5f4c5bb4a22
Jannek <jannekvieth@outlook.de>
2020-12-14 14:53:26 +0100

    Changelog
    ~ Working on Arrow behaviour while being shot

--- Praktisch/For Kingdom And Glory/Assets/Scripts/NPCs/Archer/Arrow.cs
+++ Praktisch/For Kingdom And Glory/Assets/Scripts/NPCs/Archer/Arrow.cs
@@ -7,15 +7,29 @@ public class Arrow : MonoBehaviour
     private int m_Damage = 1;
     [SerializeField]
     private AudioSource m_Source;
+    [SerializeField]
+    private GameObject m_Arrow;
 
     private float m_Timer = 0.0f;
     private GameObject m_Parent;
+    private Rigidbody2D m_Rigid;
 
     public GameObject Parent { get => m_Parent; set => m_Parent = value; }
+
+    private void Start()
+    {
+        m_Rigid = GetComponent<Rigidbody2D>(); 
+    }
 
     private void Update()
     {
         m_Timer += Time.deltaTime;
+
+        Vector2 dir = m_Rigid.velocity;
+
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+        m_Arrow.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
         if (m_Timer >= 5.0f)
             Destroy(this.gameObject);
